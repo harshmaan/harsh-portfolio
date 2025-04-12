86cbd20d86f0e8fe32969e580e00401d59234356
Harsh Maan <32389454+harshmaan@users.noreply.github.com>
2025-04-13 04:38:27 +0530

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,10 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
-  email: "contact@gianmarcocavallo.com<",
+  github: "https://github.com/harshmaan",
+  linkedin: "https://www.linkedin.com/in/harsh-maan-6b5727178/",
+  medium: "",
+  discord: "https://discord.gg/gDyUNkJn",
+  dribble: "",
+  email: "maanharsh98@hotmail.com<",
 };
 
 export const loaderAnimation = [
