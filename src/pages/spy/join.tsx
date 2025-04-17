db7189bac37e3986a40c8a86942067b5055e5808
Harsh Maan <32389454+harshmaan@users.noreply.github.com>
2025-04-17 23:33:29 +0530

    Update and rename join.tsx to join.astro

--- src/pages/spy/join.tsx
+++
@@ -1,8 +0,0 @@
----
-import JoinSpyGame from "../../components/JoinSpyGame.tsx";
-import BasicLayout from "../../layouts/BasicLayout.astro";
----
-
-<BasicLayout title="Spy Among Prompts" description="A hidden role game powered by LLMs">
-  <JoinSpyGame client:load />
-</BasicLayout>
